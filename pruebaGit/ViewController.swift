436cf4eff34e7b5d9e6e1e2aa8e96295e66bbbc7
José Juan Nogales <josejuan@mbp-de-jose.lan>
2015-11-25 23:58:40 -0600

    Initial Commit

---
+++ pruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGit
+//
+//  Created by José Juan Nogales on 25/11/15.
+//  Copyright © 2015 José Juan Nogales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
